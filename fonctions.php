21570d14c95b69c93f75ef4906e9021357dd5c6c
edwin atride <tonemail@gmail.com>
2026-03-13 10:44:59 +0100

    edwin

--- fonctions.php
+++ fonctions.php
@@ -28,7 +28,7 @@ require_once("get-proxy.php");// au lycée pour faire des requêtes https vous a
 
       /*films par genre - Action 28 - Adventure  12 - Animation 16 - Comedy 35 - Crime  80 -Documentary 99
       Drama 18 - Family 10751 - Fantasy 14- History 36 - Horror 27 - Music 10402 - Science Fiction 878
-      Thriller  53 - War 10752 - Western 37*/
+      Thriller  53 - War 10752 - Western 37*/ 
       function filmParGenre($id){
         $key = "9e43f45f94705cc8e1d5a0400d19a7b7";
 
