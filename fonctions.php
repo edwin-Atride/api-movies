4a72fca704fdfe28d9479a8dcab7723a442cc303
Varol78 <makandavarol@gmail.com>
2026-03-13 10:26:42 +0100

    creation du projet api

---
+++ fonctions.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once("get-proxy.php");// au lycée pour faire des requêtes https vous avons besoin d'indiquer le proxy
+
+
+    //fonction qui retourne dans un tableau asociatif les 20 films les plus populaires 
+    function popularMovies(){
+        $key = "9e43f45f94705cc8e1d5a0400d19a7b7";
+        $url = "https://api.themoviedb.org/3/movie/popular?api_key=$key&language=fr-FR";
+        $response = getProxy($url);
+        //$response = file_get_contents("https://api.themoviedb.org/3/movie/popular?api_key=$key&language=fr-FR");
+       
+        $result = json_decode($response, true);
+        return $result['results'];
+      }
+
+
+      function topRatedMovies(){
+        $key = "9e43f45f94705cc8e1d5a0400d19a7b7";
+        $url = "https://api.themoviedb.org/3/movie/top_rated?api_key=$key&language=fr-FR";
+        $response = getProxy($url);
+        //$response = file_get_contents("https://api.themoviedb.org/3/movie/popular?api_key=$key&language=fr-FR");
+       
+        $result = json_decode($response, true);
+        return $result['results'];
+      }
+
+
+      /*films par genre - Action 28 - Adventure  12 - Animation 16 - Comedy 35 - Crime  80 -Documentary 99
+      Drama 18 - Family 10751 - Fantasy 14- History 36 - Horror 27 - Music 10402 - Science Fiction 878
+      Thriller  53 - War 10752 - Western 37*/
+      function filmParGenre($id){
+        $key = "9e43f45f94705cc8e1d5a0400d19a7b7";
+
+        $url = "https://api.themoviedb.org/3/discover/movie?api_key=$key&language=fr-FR&with_genres=$id";
+      $response = getProxy($url);
+      //$response = file_get_contents("https://api.themoviedb.org/3/discover/movie?api_key=$key&language=fr-FR&with_genres=16");
+      $result = json_decode($response, true);
+      return $result['results'];
+     
+    }
+
+;
+    
+
+      function view(){
+        $key = "9e43f45f94705cc8e1d5a0400d19a7b7";
+        $movieId = 76600; // Ex: ID du film "Avatar"
+        $url = "https://api.themoviedb.org/3/movie/$movieId?api_key=$key&language=fr-FR";
+        $response = getProxy($url);
+        //$response = file_get_contents("https://api.themoviedb.org/3/movie/popular?api_key=$key&language=fr-FR");
+       
+        $result = json_decode($response, true);
+        return $result['results'];
+      }
+?>
+
---
+++ genreMovies.php
@@ -0,0 +1,36 @@
+
+
+
+
+<?php require("header.php"); ?>
+<?php require("fonctions.php"); ?>
+
+<?php
+if ( isset($_GET['id']) AND !empty($_GET['id'])){ 
+    $id = $_GET['id'];
+    $genres = filmParGenre($id); 
+}
+?>
+
+<div class="album py-5 bg-body-tertiary">
+  <div class="container">
+       <h4>Films Par genre</h4>
+     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+     <?php foreach($genres as $movie) : ?>  
+        <div class="d-flex align-items-stretchl">
+          <div class="card shadow-sm ">
+            <img src="<?php echo 'https://image.tmdb.org/t/p/w780/'.$movie['poster_path']; ?>" >
+            <div class="card-body lh-sm d-flex flex-column">
+              <p class=" lh-sm">
+                <strong><?php echo $movie['title']; ?></strong>
+              </p>
+             <button type="button" class="btn btn-primary mt-auto">View</button>
+            </div>
+          </div>
+        </div>
+     <?php endforeach; ?>
+    </div>
+  </div>
+</div>
+
+<?php require("footer.php"); ?>
